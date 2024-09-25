af8f8790bb1d40df71448b0c8fb637dd9c3dfac6
opilochka <olavalieva67637@gmail.com>
2024-09-25 21:57:58 +0300

    add bank

---
+++ Project1/Project1.Core/Models/Bank.cs
@@ -0,0 +1,38 @@
+﻿namespace Project1.Core.Models
+{ 
+    /// <summary>
+    /// Сущность банк
+    /// </summary>
+    public class Bank
+    {
+        private readonly Blockchain[] _blockchains; // Список блокчейнов
+        private readonly Random _random;
+        private readonly int NUM_BLOCKCHAINS = 10; // Число блокчейнов в списке
+
+        public Bank()
+        {
+            _blockchains = new Blockchain[NUM_BLOCKCHAINS];
+            for (int i = 0; i < NUM_BLOCKCHAINS; i++)
+            {
+                _blockchains[i] = new Blockchain();
+            }
+            _random = new Random();
+        }
+
+        /// <summary>
+        /// Метод для случайной записи транзакции в цепочку блокчейна
+        /// </summary>
+        /// <param name="clientId">Индефикатор клиента, который совершает транзакцию</param>
+        /// <param name="amount">Сумма транзакции, если снял, то amount отрицательный, если положил, то наоборот</param>
+        public void PerformTransaction(Guid clientId, int amount)
+        {
+            int blockchainIndex = _random.Next(NUM_BLOCKCHAINS);
+            Transaction transaction = new Transaction()
+            {
+                ClientId = clientId,
+                Amount = amount,
+            };
+            _blockchains[blockchainIndex].AddTransaction(transaction);
+        }
+    }
+}

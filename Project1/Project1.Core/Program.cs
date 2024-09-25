a4739f7bd528229e5c8f26bce38879612fbc05f4
opilochka <olavalieva67637@gmail.com>
2024-09-25 22:34:33 +0300

    add main

--- Project1/Project1.Core/Program.cs
+++ Project1/Project1.Core/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Project1.Core.Models;
+using Project1.Core.Service;
+
+class Program
+{
+    static readonly int NUM_CLINTENTS = 1000000;
+    static readonly Random Random = new Random();
+    static readonly Rmd rmd = new Rmd();
+    static void Main()
+    {
+        Bank bank = new Bank();
+        List<Client> clients = new List<Client>();
+
+        for (int i = 0; i < NUM_CLINTENTS; i++)
+        {
+            Client client = new Client()
+            {
+                Score = Random.Next()
+            };
+            clients.Add(client);
+        }
+
+        for (int i = 0;i < NUM_CLINTENTS; i++)
+        {
+            var money = Random.Next();
+            if (rmd.FalseOrTrue()) 
+            {
+                bank.PerformTransaction(clients[i].Id, money);
+                clients[i].DepositingMoney(money);
+            }
+            else
+            {
+                bank.PerformTransaction(clients[i].Id, -money);
+                clients[i].WithdrawalMoney(money);
+            }
+        }
+        Console.WriteLine("Все транзакции выполнены.");
+    }
+}

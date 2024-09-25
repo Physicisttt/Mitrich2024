a0cf86cb77ca3f8938d90d42545f0682c7484fda
opilochka <olavalieva67637@gmail.com>
2024-09-25 21:28:33 +0300

    add transaction

---
+++ Project1/Project1.Core/Models/Transaction.cs
@@ -0,0 +1,13 @@
+﻿namespace Project1.Core.Models
+{
+    public class Transaction
+    {
+        public Guid Id { get; set; }
+
+        public Guid ClientId { get; set; }
+
+        public int Amount { get; set; }
+
+        public DateTime DateTime { get; set; }
+    }
+}

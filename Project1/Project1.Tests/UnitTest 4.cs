4ef8e46b1aef278f7b42cc7de95c694b02980fb9
mon1gol <profile4634@yandex.ru>
2024-10-06 21:36:30 +0300

    added unit test 4: perform transaction change score correctly

---
+++ Project1/Project1.Tests/UnitTest 4.cs
@@ -0,0 +1,31 @@
+﻿using Project1.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1.Tests
+{
+    internal class UnitTest_4
+    {
+        [Test]
+        public void Test_PerformTransaction_ChangeScoreCorrectly()
+        {
+            var client = setClientObject();
+            int original_score = client.Score;
+            var bank = new Bank();
+            bank.PerformTransaction(client);
+            Assert.That(client.Score, Is.Not.EqualTo(original_score));
+        }
+        private static Client setClientObject()
+        {
+            return new()
+            {
+                Id = Guid.NewGuid(),
+                Score = 1000
+            };
+        }
+    }
+}
